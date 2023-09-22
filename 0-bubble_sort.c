18f82d27c1376da50a66889637fab479419421f5
d12o6aa <doaakarem798@gmail.com>
2023-09-22 16:27:36 +0300

    first function in task

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - sorted an array
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
