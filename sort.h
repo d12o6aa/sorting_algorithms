f8e010cd6e98e14ca0daad3f892a8dce7e69f162
d12o6aa <doaakarem798@gmail.com>
2023-09-26 00:20:03 +0300

    update header file

--- sort.h
+++ sort.h
@@ -25,5 +25,8 @@ void bubble_sort(int *array, size_t size);
 void swap(int *i, int *j);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+int partition(int *array, int first, int last, size_t size);
+void qs(int *array, int first, int last, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
